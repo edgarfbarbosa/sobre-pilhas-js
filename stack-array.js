c9f28becd5f16a9dfd72c7a92b46f8e44dddc7d1
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-24 11:10:10 -0300

    feat(Stack): adiciona os métodos push, pop e peek

--- stack-array.js
+++ stack-array.js
@@ -2,4 +2,19 @@ class Stack {
   constructor() {
     this.items = []
   }
+  
+  /** Método responsável pela adição de novos elementos no topo da pilha */
+  push(element) {
+    this.items.push(element)
+  }
+  
+  /** Método responsável pela remoção do último elemento adicionado ao topo da pilha */
+  pop() {
+    return this.items.pop()
+  }
+  
+  /** Método responsável por devolver o item que está no topo da pilha */
+  peek() {
+    return this.items[this.items.length - 1]
+  }
 }
