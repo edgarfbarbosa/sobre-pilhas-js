0e63b3488bca2e30ca40f6c26359b963a46753e9
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-25 10:16:42 -0300

    add(stack.js): classe Stack baseada em objeto

---
+++ stack.js
@@ -0,0 +1,8 @@
+class Stack {
+  constructor() {
+    /** this.count é uma propriedade responsável por ajudar a manter o controle do tamanho da pilha */
+    this.count = 0
+    this.items = []
+  }
+  
+}
