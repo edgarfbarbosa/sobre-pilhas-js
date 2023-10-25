0a732aa5a552cb9aac72d38398d2d55658a89069
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-25 09:42:45 -0300

    feat(Stack): adiciona os métodos isEmpty, size e clear

--- stack-array.js
+++ stack-array.js
@@ -8,7 +8,7 @@ class Stack {
     this.items.push(element)
   }
   
-  /** Método responsável pela remoção do último elemento adicionado ao topo da pilha */
+  /** Método responsável por devolver e remover o último elemento adicionado ao topo da pilha */
   pop() {
     return this.items.pop()
   }
@@ -16,5 +16,20 @@ class Stack {
   /** Método responsável por devolver o item que está no topo da pilha */
   peek() {
     return this.items[this.items.length - 1]
+  }
+  
+  /** Método responsável por retornar true se a pilha estiver vazia e false caso o tamanho da pilha for maior que 0 */
+  isEmpty() {
+    return this.items.length === 0
+  }
+  
+  /** Método responsável por verificar o número de elementos na pilha */
+  size() {
+    return this.items.length
+  }
+  
+  /** Método responsável pela remoção de todos os elementos */
+  clear() {
+    this.items = []
   }
 }
